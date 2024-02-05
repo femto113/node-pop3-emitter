118158720c6513884fc3deeed21fabf14f02d0db
Jorge Pacora <jpacora@outlook.com>
2024-02-05 16:18:04 -0500

    Fix undefined which

--- index.js
+++ index.js
@@ -380,6 +380,7 @@ POP3Connection.prototype.commands = {
       if (which && !uids.length) {
         this.respondErr('no such message');
       } else if (uids.length == 1) {
+        which = (typeof which == "undefined") ? 1 : which
         this.respondOk(which + ' ' + uids[0]);
       } else {
         // TODO: create a "send_enumeration" utility for these
